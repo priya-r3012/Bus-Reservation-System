15934b9cbf85cdbcd0118a452282f13e1f1a2939
priya-r3012 <priyarohilla816@gmail.com>
2026-02-05 16:38:17 +0530

    dictionary

---
+++ Biodata python/program python/dictionary.py
@@ -0,0 +1,19 @@
+
+print("\nMarks for subjects: ")
+math_marks = int(input("\nEnter Math marks: "))
+science_marks = int(input("Enter Science marks: "))
+hindi_marks = int(input("Enter Hindi marks: "))
+ss_marks = int(input("Enter SS marks: "))
+english_marks = int(input("Enter English marks: "))
+
+
+marks = {
+    'Math': math_marks,
+    'Science': science_marks,
+    'Hindi': hindi_marks,
+    'SS': ss_marks,
+    'English': english_marks
+}
+
+
+print("\nDictionary of subject marks:", marks)
